baf42784b95cd4a9077c12c45bddbb503403a3b2
store <jeet8halder@gmail.com>
2021-05-18 10:47:32 +0530

    updated for prod env

--- pixzium/settings.py
+++ pixzium/settings.py
@@ -205,20 +205,25 @@ LOGOUT_REDIRECT_URL = '/'
 
 # AWS Settings
 
+# AWS Settings
+
 # AWS_LOCATION = 'static'
 # AWS_ACCESS_KEY_ID = config['AWS_ACCESS_KEY']  # Your AWS Access Key ID
 # AWS_SECRET_ACCESS_KEY = config['AWS_SECRET_KEY']  # Your AWS Secret Access Key
 # AWS_STORAGE_BUCKET_NAME = config['AWS_BUCKET_NAME']  # Your AWS Bucket name
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# #AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_CLOUDFRONT_DOMAIN = 'd3gxnqjvuiz5a8.cloudfront.net'
 # AWS_S3_OBJECT_PARAMETERS = {
 #     'CacheControl': 'max-age=86400',
 # }
-# DEFAULT_FILE_STORAGE = 'wtGallery.storage_backends.MediaStorage'
+# DEFAULT_FILE_STORAGE = 'pixzium.storage_backends.MediaStorage'
 # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#
+#
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CLOUDFRONT_DOMAIN, AWS_LOCATION)
 # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # STATICFILES_FINDERS = (
 #     'django.contrib.staticfiles.finders.FileSystemFinder', 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
